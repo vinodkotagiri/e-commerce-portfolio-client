ca1f70ef86c2eaa006ea3e4751fb9c0c2c1e544a
vinodkotagiri <vinodkotagiri@icloud.com>
2022-12-30 11:20:15 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import { Col, Container, Row } from 'react-bootstrap'
 
 export default function Footer() {
-	return <div>Footer</div>
+	return (
+		<footer style={{ position: 'absolute', bottom: 0, width: '100%' }}>
+			<Container fluid className='bg-dark text-white text-center py-5'>
+				<Row>
+					<Col>Copyright &copy; S-Cart</Col>
+				</Row>
+			</Container>
+		</footer>
+	)
 }
